31834f738d0ef8bbe3659dec3984b69d1bf82b01
BrightX0 <augustinobright@gmail.com>
2022-11-26 21:31:16 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 
 function App() {
+  
+  const firstName = "Toto";
+  const lastname = "Bright";
+
+  const getfullName = () => {
+    return `${firstName} ${lastname}`;
+  }
+
   return (
     <div className="App">
-      <h3>Full Name: JOhn Wick Lark</h3>
+      <h3>FullName: {getfullName()}</h3>
       <p>Age: 23</p>
       <p>Job: Anonymous</p>
+
+      <input placeholder='Enter Data' />
     </div>
+
   );
 }
 
